2dfd9512fb80e82a01eb4d2467d389b10759e668
nikhil95301 <nikhil.anandkumar@couchbase.com>
2024-09-05 17:37:10 +0530

    Changing the skip_dataload type

--- vectordb_bench/backend/clients/couchbase/config.py
+++ vectordb_bench/backend/clients/couchbase/config.py
@@ -109,7 +109,7 @@ class CouchbaseGSICVIndexConfig(CouchbaseIndexConfig):
     train_list: int | None = None
     scan_nprobes: int | None = None
     vector_similarity: str = "L2"
-    skip_dataload: bool = False
+    skip_dataload: str = None
 
     def parse_metric(self) -> str:
         return self.metric_type.value
